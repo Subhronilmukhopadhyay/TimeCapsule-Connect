c09819e64755d15def6fce482773f7d8dc1455a2
Subhronilmukhopadhyay <135453663+Subhronilmukhopadhyay@users.noreply.github.com>
2025-07-20 10:02:58 +0530

    Update security.js

--- server/middleware/security.js
+++ server/middleware/security.js
@@ -12,18 +12,18 @@ const securityMiddleware = (app) => {
   
   app.use(cors({ 
     // origin: 'https://timecapsule-connect-1.onrender.com', //production mode
-    origin: '*', //development mode
+    origin: 'http://localhost:5173', //development mode
     credentials: true 
   }));
 
-  // const csrfProtection = csrf({ cookie: true }); // development mode
-  const csrfProtection = csrf({ //production mode
-    cookie: {
-      httpOnly: false,
-      secure: true,
-      sameSite: 'None',
-    }
-  });
+  const csrfProtection = csrf({ cookie: true }); // development mode
+  // const csrfProtection = csrf({ //production mode
+  //   cookie: {
+  //     httpOnly: false,
+  //     secure: process.env.NODE_ENV === 'production',
+  //     sameSite: 'None',
+  //   }
+  // });
 
   app.use(cookieParser());
   app.use(csrfProtection);
@@ -41,9 +41,9 @@ const securityMiddleware = (app) => {
 
     res.cookie('XSRF-TOKEN', csrfToken, {
         httpOnly: false,
-        secure: true,
-        sameSite: 'None', //production mode
-        // sameSite: 'Strict', //development mode
+        secure: process.env.NODE_ENV === 'production',
+        // sameSite: 'None', //production mode
+        sameSite: 'Strict', //development mode
         maxAge: 3600000,
     });
     res.status(200).json({ message: 'CSRF token set in cookie' });
