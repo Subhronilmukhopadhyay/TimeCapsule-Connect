2f786461f52edc185e63ac1ea2bb0ee1af02a334
Kumar-Amitesh <amitesh04kumar@gmail.com>
2025-07-22 12:21:17 +0530

    Refactor Navbar component: revert Link component for login navigation to anchor tag

--- Client/src/components/Navbar/Navbar.jsx
+++ Client/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from './Navbar.module.css';
-import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
@@ -11,7 +10,7 @@ const Navbar = () => {
           <li><a href="#hero">Home</a></li>
           <li><a href="#features">Features</a></li>
           <li><a href="#how-it-works">How It Works</a></li>
-          <li><Link to="/login">Login</Link></li>
+          <li><a href="/login">Login</a></li>
         </ul>
       </nav>
     </header>
