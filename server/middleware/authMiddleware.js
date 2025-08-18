a36dc568778faa7559d2915e0da3a915553f2d16
subhronil <subhronil124@outlook.com>
2025-08-19 00:04:40 +0530

    final final final try

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -63,8 +63,8 @@ export const loginUser = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true, 
       secure: process.env.NODE_ENV === 'production',
-      // sameSite: 'None', //production mode
-      sameSite: 'Strict', //development mode
+      sameSite: 'None', //production mode
+      // sameSite: 'Strict', //development mode
       maxAge: 3600000,
     });
 
